ae6c4870bc32bf74d43cb44f1feb9745cdaef3a5
彭永顺 <740604387@qq.com>
2016-09-08 11:12:23 +0800

    搭框架

--- app/src/main/java/com/example/u17/WelcomeActivity.java
+++ app/src/main/java/com/example/u17/WelcomeActivity.java
@@ -1,5 +1,10 @@
 package com.example.u17;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ImageView;
@@ -10,11 +15,42 @@ import butterknife.ButterKnife;
 public class WelcomeActivity extends AppCompatActivity {
     @BindView(R.id.activity_welcome_iv_welcome)
     public ImageView ivWelcome;
+    private SharedPreferences firstStartSp;
+    private Handler mHandle=new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            //获取SharedPreferences对象
+            firstStartSp = getSharedPreferences("count", Context.MODE_PRIVATE);
+            int count = firstStartSp.getInt("count", 0);
+            //判断程序与第几次运行，如果是第一次运行则跳转到引导页面
+            if (count==0){
+                Intent intent=new Intent();
+                intent.setClass(WelcomeActivity.this,GuideActivity.class);
+                startActivity(intent);
+                finish();
+            }else {
+                //转跳到首页
+                Intent intent1=new Intent();
+                intent1.setClass(WelcomeActivity.this, HomeActivity.class);
+                startActivity(intent1);
+                finish();
+            }
+            count++;
+            //获取编辑器
+            SharedPreferences.Editor editor=firstStartSp.edit();
+            //存入内部存储中
+            editor.putInt("count",count);
+            editor.commit();
+        }
+
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome);
         ButterKnife.bind(this);
+        mHandle.sendEmptyMessageDelayed(1,2000);
     }
 }
