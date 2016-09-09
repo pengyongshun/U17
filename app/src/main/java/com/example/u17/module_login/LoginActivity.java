df22501cffddc92a95463dd55c790e651960d250
King1033 <1786551559@qq.com>
2016-09-09 17:55:21 +0800

    添加在未登录的情况下，每个点击所跳转的各个子布局

---
+++ app/src/main/java/com/example/u17/module_login/LoginActivity.java
@@ -0,0 +1,20 @@
+package com.example.u17.module_login;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.u17.R;
+
+/**
+ * @Author:king1033
+ * @Desc:
+ * @Time:2016/9/9
+ */
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
+        super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_login);
+    }
+}
---
+++ app/src/main/java/com/example/u17/module_mine/url/GameUrl.java
@@ -0,0 +1,9 @@
+package com.example.u17.module_mine.url;
+
+/**
+ * @Author:king1033
+ * @Desc:
+ * @Time:2016/9/9
+ */
+public class GameUrl {
+}
